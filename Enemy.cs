d9f5ebb18e1cc8d6666a2fe5cfd73baa345c9ae5
namanh2323 <namanhnamanhgithub@gmail.com>
2020-11-26 17:30:57 +0700

    bot

---
+++ Enemy.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    int enemyHealth;
+
+    public Megaman player;
+
+    public Rigidbody2D r2;
+    public Animator anim;
+
+    public bool faceright = true;
+    public float distanceToLeft, distanceToRight;
+    public float distance;
+    public float wakerange;
+    public Transform shootpointL, shootpointR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Megaman>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        distanceToLeft = Vector2.Distance(shootpointL.position, player.transform.position);
+        distanceToRight = Vector2.Distance(shootpointR.position, player.transform.position);
+        if (distance < agroRange)
+        {
+            //chase Megaman
+            ChasingPlayer();
+        }
+
+        if (distance > agroRange)
+        {
+            //stop chase
+            StopChasingPlayer();
+        }
+    }
+
+    public void ChasingPlayer()
+    {
+        if (transform.position.x < transform.position.x)
+        {
+            r2.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            r2.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+        anim.Play("EnemyRotate");
+    }
+
+    public void StopChasingPlayer()
+    {
+        r2.velocity = new Vector2(0, 0);
+        anim.Play("EnemyIdle");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Bullet"))
+        {
+            Destroy(col.gameObject);
+            enemyHealth--;
+            if (distance > agroRange)
+            {
+                gameObject.GetComponent<Animation>().Play("EnemyIdleRed");
+            }
+            else if (distance < agroRange)
+            {
+                gameObject.GetComponent<Animation>().Play("EnemyRotateRed");
+            }
+            if (enemyHealth == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (col.CompareTag("Player"))
+        {
+            player.Damage(2);
+            player.Knockback(350f, player.transform.position);
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        enemyHealth -= damage;
+
+    }
+
+    public void Knockback(float Knockpow, Vector2 Knockdir)
+    {
+        r2.velocity = new Vector2(0, 0);
+        r2.AddForce(new Vector2(Knockdir.x * -100, Knockdir.y * Knockpow));
+    }
+}
---
+++ TurretAI.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    public int curHealth = 10;
+    public float distanceToLeft, distanceToRight;
+    public float distance;
+    public float wakerange;
+    public float shootinterval;
+    public float bulletspeed = 5;
+    public float bullettimer;
+
+    public bool awake = false;
+    public bool lookingRight = true;
+
+    public GameObject bullet;
+    public Transform target;
+    public Animator anim;
+    public Transform shootpointL, shootpointR;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Awake", awake);
+        anim.SetBool("LookRight", lookingRight);
+
+        RangeCheck();
+
+        if (target.transform.position.x > transform.position.x)
+        {
+            lookingRight = true;
+        }
+
+        if (target.transform.position.x < transform.position.x)
+        {
+            lookingRight = false;
+        }
+
+        if (curHealth < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(1);
+        yield return 0;
+    }
+    void RangeCheck()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        distanceToLeft = Vector2.Distance(shootpointL.position, target.transform.position);
+        distanceToRight = Vector2.Distance(shootpointR.position, target.transform.position);
+        if (distance < wakerange)
+        {
+            awake = true;
+            if (distanceToLeft < distanceToRight)
+            {
+                delay();
+                Attack(false);
+            }
+            if (distanceToRight < distanceToLeft)
+            {
+                delay();
+                Attack(true);
+            }
+        }
+
+        if (distance > wakerange)
+        {
+            awake = false;
+
+        }
+    }
+
+    public void Attack(bool attackright)
+    {
+        bullettimer += Time.deltaTime;
+
+        if (bullettimer >= shootinterval)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            if (attackright)
+            {
+                GameObject bulletclone;
+                bulletclone = Instantiate(bullet, shootpointR.transform.position, shootpointR.transform.rotation) as GameObject;
+                bulletclone.GetComponent<Rigidbody2D>().velocity = direction * bulletspeed;
+
+                bullettimer = 0;
+            }
+
+            if (!attackright)
+            {
+                GameObject bulletclone;
+                bulletclone = Instantiate(bullet, shootpointL.transform.position, shootpointL.transform.rotation) as GameObject;
+                bulletclone.GetComponent<Rigidbody2D>().velocity = direction * bulletspeed;
+
+                bullettimer = 0;
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Bullet")) {
+            Destroy(col.gameObject);
+            curHealth--;
+            gameObject.GetComponent<Animation>().Play("TurretRedFlash");
+
+            if (curHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
